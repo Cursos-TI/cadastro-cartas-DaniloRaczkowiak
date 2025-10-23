7782aac03bef0c2e14b0e158c4195238367293ab
DaniloRaczkowiak <contato@dlautomacoes.com.br>
2025-10-22 23:33:46 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,89 @@
 #include <stdio.h>
+int main (){
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+// criação das variaveis para armazenar as informações fornecidas.
+// variaveis carta 01
+    char estado1;
+    char cidade1[10], codcarta1[6];
+    int  populacao1, pontos1;
+    float area1, pib1;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+// variaveis carta 02
+    char estado2, codcarta2[6], cidade2[10];
+    int populacao2, pontos2;
+    float area2, pib2;
+//inicio do jogo.
+    printf("Jogo Super Trunfo.\n");
+    printf("Cadastre as cartas do jogo...\n");
 
-  // Área para entrada de dados
+// cadastro da carta 01.
+    printf("Cadastro da carta 01.\n");
+    printf("insira uma letra de A a H para representar o Estado:\n");
+    scanf("%c", &estado1);
+    printf("insira um codigo de 01 a 04 para essa carta:\n");
+    scanf("%s", &codcarta1);
+    printf("insira o nome da cidade:\n");
+    scanf("%s", &cidade1);
+    printf("insira a população:\n");
+    scanf("%d", &populacao1);
+    printf("insira a Área:\n");
+    scanf("%f", &area1);
+    printf("insira o PIB:\n");
+    scanf("%f", &pib1);
+    printf("insira o numero de pontos turisticos:\n");
+    scanf("%d", &pontos1);
 
-  // Área para exibição dos dados da cidade
+    printf("Cadastro da carta 01 finalizado! vamos para a proxima carta...\n");
 
-return 0;
-} 
+// cadastro da carta 02.
+
+    printf("Cadastro da carta 02.\n");
+    printf("insira uma letra de A a H para representar o Estado:\n");
+    scanf("%s", &estado2);
+    printf("insira um codigo de 01 a 04 para essa carta:\n");
+    scanf("%s", &codcarta2);
+    printf("insira o nome da cidade:\n");
+    scanf("%s", &cidade2);
+    printf("insira a população:\n");
+    scanf("%d", &populacao2);
+    printf("insira a Área:\n");
+    scanf("%f", &area2);
+    printf("insira o PIB:\n");
+    scanf("%f", &pib2);
+    printf("insira o numero de pontos turisticos:\n");
+    scanf("%d", &pontos2);
+
+        printf("Cadastro da carta 02 finalizado!\n\n");
+
+//inicio das opções que vão ser escritas na tela após o cadastro ds cartas.
+
+
+//descritivo carta 01
+    printf("Carta 1:\n");
+    printf("Estado:%c\n", estado1);
+    printf("Código:%c%s\n", estado1, codcarta1);
+    printf("Nome da Cidade:%s\n", cidade1);
+    printf("População:%d\n", populacao1);
+    printf("Área:%f KM²\n",area1);
+    printf("PIB:%f bilhões de reais\n", pib1);
+    printf("Número de pontos Turísticos: %d\n\n", pontos1);
+
+//descritivo carta 02
+    printf("Carta 2:\n");
+    printf("Estado:%c\n", estado2);
+    printf("Código:%c%s\n", estado2, codcarta2);
+    printf("Nome da Cidade:%s\n", cidade2);
+    printf("População:%d\n", populacao2);
+    printf("Área:%f KM²\n",area2);
+    printf("PIB:%f bilhões de reais\n",pib2);
+    printf("Número de pontos Turísticos: %d\n\n", pontos2);
+
+
+//mensagem do final do programa
+printf("FIM DO PROGRAMA!!!");
+
+
+
+
+
+}  
